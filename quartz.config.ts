79c2ae53e348bad7f07eb53c291d77f800c834ab
AndreyID <dai@devision.company>
2026-04-24 14:25:47 +0300

    vault backup: 2026-04-24 14:25:47

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "DEVISION",
+    pageTitle: "Devision",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
